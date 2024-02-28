aeda0d3896961b9b5345fa9746a5aa13da871ba6
RyanGui035 <160800820+RyanGui035@users.noreply.github.com>
2024-02-27 21:50:26 -0300

    Add files via upload

---
+++ Ryan Guilherme/projeto2/processa.php
@@ -0,0 +1,22 @@
+<?php 
+
+
+$nome=$_GET["nome"];
+$email=$_GET["email"];
+$mensagem=$_GET["mensagem"];
+$sexo=$_GET["sexo"];
+
+echo "NOME: $nome <br>";
+echo "EMAIL: $email <br>";
+echo "MENSAGEM: $mensagem <br>";
+echo "SEXO: $sexo <br>";
+
+if(isset($_GET["curso"])){
+    $curso=$_GET["curso"];
+    foreach($curso as $item){
+        echo "CURSO: $item <br>";
+}
+}
+
+
+?>
